2b36a9d31a5d2cbb8949a1cbabc45e17a0ca142e
Andreas Christensen <Andreasdebel@hotmail.com>
2025-03-03 11:38:21 +0100

    migrated project to minimal web api

--- KredditAPI/Data/EntryContext.cs
+++ KredditAPI/Data/EntryContext.cs
@@ -1,8 +1,9 @@
 using System;
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
+using Model;
 
-namespace Model
+namespace KredditAPI.Data
 {
     public class EntryContext : DbContext
     {
---
+++ KredditWebAPI/Model/Entry.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Entry
+
+    {
+        public Entry(string titel, string text, DateTime date, string author, int votes) 
+        {
+            this.Titel = titel;
+            this.Text = text;
+            this.Date = date;
+            this.Author = author;
+            this.Votes = votes;
+            this.CommentList = new List<Comment>();
+        }
+        public long EntryId { get; set; }
+
+        public string? Titel { get; set; }
+
+        public string? Text { get; set; }
+
+        public DateTime? Date { get; set; }
+
+        public string? Author { get; set; }
+
+        public int Votes { get; set; }
+
+        public List<Comment> CommentList  { get; set; }
+
+
+    
+    }
+}
